9a3dbd484aec75f17d4484aaf22f444e9c295740
akbarrfauzann <makbarfauzans@gmail.com>
2025-01-25 18:56:13 +0700

    update

--- src/pages/MovieDetails/MovieRecommendations.jsx
+++ src/pages/MovieDetails/MovieRecommendations.jsx
@@ -1,16 +1,22 @@
-const MovieRecommendations = ({ recommendations }) => (
-  <div className="text-center">
-    <h1 className="pb-4 text-white font-bold">Recommendation Movies</h1>
-    {recommendations.map((recommendation) => (
-      <img
-        key={recommendation.id}
-        src={`https://image.tmdb.org/t/p/w500${recommendation.poster_path}`}
-        alt={recommendation.title}
-        className="mx-auto mb-6 w-48 cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
-        onClick={() => (window.location.href = `/movies/${recommendation.id}`)}
-      />
-    ))}
-  </div>
-);
+import { useNavigate } from "react-router-dom";
+
+const MovieRecommendations = ({ recommendations }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="text-center">
+      <h1 className="pb-4 text-white font-bold">Recommendation Movies</h1>
+      {recommendations.map((recommendation) => (
+        <img
+          key={recommendation.id}
+          src={`https://image.tmdb.org/t/p/w500${recommendation.poster_path}`}
+          alt={recommendation.title}
+          className="mx-auto mb-6 w-48 cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+          onClick={() => navigate(`/movies/${recommendation.id}`)} // Ganti window.location.href dengan navigate
+        />
+      ))}
+    </div>
+  );
+};
 
 export default MovieRecommendations;
